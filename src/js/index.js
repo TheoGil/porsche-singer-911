cbe0423c98106b853228a79ddd3bc3b0402079ec
= <=>
2020-02-06 16:16:09 +0100

    fix FOUC

--- src/js/index.js
+++ src/js/index.js
@@ -52,70 +52,67 @@ gsap.set(footerEl, {
   scaleX: 0
 });
 
-// WRAP OUR INIT WITHIN STO BECAUSE FOUC MESS UP OUR CALCULATIONS
-window.setTimeout(() => {
-  const slideshow = new Slideshow({
-    debug: true,
-    uniforms: {},
-    intensity: 0.1,
-    transitionDuration: 0.5,
-    autoplayDuration: 8,
-    ease: "power2.out",
-    // NOTE: Currently only works if all images have the same aspect ratio.
-    // Refer to the resize function within the Sketch class
-    images: [
-      "/public/slides/1.jpg",
-      "/public/slides/2.jpg",
-      "/public/slides/3.jpg"
-    ],
-    containerId: "js-slideshow-container",
-    canvasId: "slideshow",
-    thumbnailsClass: "js-thumbnail",
-    nextClass: "js-next",
-    prevClass: "js-prev",
-    fragment
+const slideshow = new Slideshow({
+  debug: true,
+  uniforms: {},
+  intensity: 0.1,
+  transitionDuration: 0.5,
+  autoplayDuration: 8,
+  ease: "power2.out",
+  // NOTE: Currently only works if all images have the same aspect ratio.
+  // Refer to the resize function within the Sketch class
+  images: [
+    "/public/slides/1.jpg",
+    "/public/slides/2.jpg",
+    "/public/slides/3.jpg"
+  ],
+  containerId: "js-slideshow-container",
+  canvasId: "slideshow",
+  thumbnailsClass: "js-thumbnail",
+  nextClass: "js-next",
+  prevClass: "js-prev",
+  fragment
+});
+const header = new Header();
+header.animateIn().then(() => {
+  // Animate in "basic stuff"
+  TL.to(staggerEls, {
+    opacity: 1,
+    y: 0,
+    duration: 0.5,
+    stagger: {
+      amount: 1
+    }
   });
-  const header = new Header();
-  header.animateIn().then(() => {
-    // Animate in "basic stuff"
-    TL.to(staggerEls, {
-      opacity: 1,
-      y: 0,
-      duration: 0.5,
+
+  // Animate slideshow and thumbnails
+  const curtainsAnimationOffset = 0.5;
+  TL.to(
+    curtainEls,
+    {
       stagger: {
-        amount: 1
+        y: 0,
+        amount: 1,
+        onStart: function() {
+          const target = this._targets[0];
+          cancelTween(target, "opacity,y");
+          curtainAnimation(target);
+        }
       }
-    });
+    },
+    `-=${curtainsAnimationOffset}`
+  );
 
-    // Animate slideshow and thumbnails
-    const curtainsAnimationOffset = 0.5;
-    TL.to(
-      curtainEls,
-      {
-        stagger: {
-          y: 0,
-          amount: 1,
-          onStart: function() {
-            const target = this._targets[0];
-            cancelTween(target, "opacity,y");
-            curtainAnimation(target);
-          }
-        }
-      },
-      `-=${curtainsAnimationOffset}`
-    );
-
-    // Animate footer
-    TL.to(
-      footerEl,
-      {
-        duration: 0.5,
-        scaleX: 1,
-        onComplete: () => {
-          tweenCSSVar("--gradient-opacity", footerEl, 0.5, 0, 0.08);
-        }
-      },
-      `-=${curtainsAnimationOffset}`
-    );
-  });
-}, 100);
+  // Animate footer
+  TL.to(
+    footerEl,
+    {
+      duration: 0.5,
+      scaleX: 1,
+      onComplete: () => {
+        tweenCSSVar("--gradient-opacity", footerEl, 0.5, 0, 0.08);
+      }
+    },
+    `-=${curtainsAnimationOffset}`
+  );
+});
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,6 +2,7 @@ const Path = require("path");
 const Webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -15,6 +16,9 @@ module.exports = merge(common, {
   plugins: [
     new Webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("development")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
     })
   ],
   module: {
@@ -35,7 +39,7 @@ module.exports = merge(common, {
       },
       {
         test: /\.s?css$/i,
-        use: ["style-loader", "css-loader?sourceMap=true", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   }
